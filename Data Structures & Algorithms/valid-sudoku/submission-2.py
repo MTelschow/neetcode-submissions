b94253c818616f3cf6eeaf83fd092b41789c72de
Mathis Telschow <mathistelschow@gmail.com>
2026-04-23 14:32:56 +0200

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def checkRows(self, board):
+        for rowIdx in range(9):
+            seen = set()
+            for colIdx in range(9):
+                current = board[rowIdx][colIdx]
+                if current == ".":
+                    continue
+                if current in seen:
+                    return False
+                seen.add(current)
+        return True
+
+    def checkCols(self, board):
+        for colIdx in range(9):
+            seen = set()
+            for rowIdx in range(9):
+                current = board[rowIdx][colIdx]
+                if current == ".":
+                    continue
+                if current in seen:
+                    return False
+                seen.add(current)
+        return True
+
+    def checkSqaures(self, board):
+        for squareIdx in range(9):
+            seen = set()
+
+            corner_y = (squareIdx // 3) * 3
+            corner_x = (squareIdx % 3) * 3
+
+            for colIdx in range(3):
+                for rowIdx in range(3):
+                    current = board[corner_y + rowIdx][corner_x + colIdx]
+                    if current == ".":
+                        continue
+                    if current in seen:
+                        return False
+                    seen.add(current)
+        return True
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        if not self.checkRows(board):
+            return False
+        if not self.checkCols(board):
+            return False
+        return self.checkSqaures(board)
